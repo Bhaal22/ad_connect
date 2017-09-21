6f79a9d21003b05a750dad5f86a4b6b3e0127d52
Bhaal22 <muller.john@gmail.com>
2017-09-21 13:34:14 +0200

    fixed parameters ordering

--- Program.cs
+++ Program.cs
@@ -1,19 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ad_connect
+﻿namespace ad_connect
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var domain = args[0];
-            var username = args[1];
-            var password = args[2];
-            var delegatedDomain = args[3];
+            var delegatedDomain = args[0];
+            var domain = args[1];
+            var username = args[2];
+            var password = args[3];
             var group = args[4];
 
             GroupMemberShip.IsMemberOf(delegatedDomain, domain, username, password, group);
